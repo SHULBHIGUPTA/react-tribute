0099c9e32a2f9070b4845a1ef062d15fb07dc6b6
SHULBHIGUPTA <shulbhi18@gmail.com>
2022-02-13 05:51:27 -0800

    updates

---
+++ src/Tribute.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import './App.css';
+
+class Tribute extends Component{
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return(
+            <div className="container">
+                <img src = {this.props.Images}/>  
+                <h2>{this.props.Text}</h2>
+                <h3>{this.props.biography}</h3>
+            </div>
+        )
+    }
+}
+
+export default Tribute;
